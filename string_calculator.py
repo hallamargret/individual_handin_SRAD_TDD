707d84cfd4932d7a2771068ed9cc18d63c588f74
hallaj19 <hallaj19@ru.is>
2020-03-26 15:28:27 +0000

    Add supports a single number

--- string_calculator.py
+++ string_calculator.py
@@ -1,3 +1,5 @@
 def Add(numbers):
     if numbers == "":
         return 0
+    if len(numbers) == 1:
+        return int(numbers)
--- string_calculator_tests.py
+++ string_calculator_tests.py
@@ -2,3 +2,6 @@ import string_calculator
 
 def test_empty_str():
     assert string_calculator.Add("") == 0
+
+def test_single_number():
+    assert string_calculator.Add("1") == 1
